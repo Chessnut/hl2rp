e859b0bd2d8c74c33c4e0be9ee99fece140bb310
TheOmegaSir <theomegasir@hotmail.com>
2014-04-18 17:04:11 -0430

    Update sv_hooks.lua

--- gamemode/schema/sv_hooks.lua
+++ gamemode/schema/sv_hooks.lua
@@ -314,11 +314,11 @@ function SCHEMA:GetDefaultInv(inventory, client, data)
 			})
 		end
 		
-	if (data.faction == FACTION_CP or data.faction == FACTION_OW) then
+		if (data.faction == FACTION_CP or data.faction == FACTION_OW) then
 			inventory:Add("flashlight", 1)
 		end	
 			
-	if (data.faction == FACTION_OW) then
+		if (data.faction == FACTION_OW) then
 			inventory:Add("weapon_smg1", 1, {Equipped = false, CombineLocked = 0, ClipOne = 45})
 			inventory:Add("ammo_smg", 5)
 			inventory:Add("weapon_frag", 1, {Equipped = false, CombineLocked = 0, ClipOne = -1})
