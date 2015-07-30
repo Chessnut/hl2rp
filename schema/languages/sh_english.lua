7da5f7f16b2362df69332c6e15d8711427931fb7
Brian <chessnutist@gmail.com>
2015-07-29 19:38:53 -0700

    Removed schema description translation.

--- schema/languages/sh_english.lua
+++ schema/languages/sh_english.lua
@@ -7,7 +7,10 @@ LANGUAGE = {
 	fOverwatchDesc = "Soldiers for the Universal Union.",
 	fAdminName = "Administrator",
 	fAdminDesc = "The leader of this city.",
-	schemaDesc = "Under rule of the Universal Union.",
+	-- This is an example of how you can have your schema description be translatable.
+	-- But it's commented because people keep complaining that their description isn't changing.
+	-- So this is commented since it overwrites the SCHEMA.desc
+	--schemaDesc = "Under rule of the Universal Union.",
 	mustBeCP = "You must be in the Civil Protection to do this.",
 	objectives = "Objectives",
 	notCombine = "You must be a member of the Combine to do this.",
