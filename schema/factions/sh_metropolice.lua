909b148c58d6f388a10cabf5275adb86a702cd96
Brian Hang <hangwithbrian@gmail.com>
2018-12-13 09:29:53 -0800

    Transfering to civil protection now updates name and model

--- schema/factions/sh_metropolice.lua
+++ schema/factions/sh_metropolice.lua
@@ -2,6 +2,7 @@ FACTION.name = "fCopName"
 FACTION.desc = "fCopDesc"
 FACTION.color = Color(25, 30, 180)
 FACTION.isDefault = false
+--FACTION.limit = 0.25
 FACTION.models = {
 	"models/police.mdl"
 }
@@ -9,13 +10,37 @@ FACTION.weapons = {"nut_stunstick"}
 FACTION.pay = 25
 FACTION.isGloballyRecognized = true
 
-function FACTION:onGetDefaultName(client)
+function FACTION:onGetDefaultName(client, digits)
 	if (SCHEMA.digitsLen >= 1) then
-		local digits = math.random(tonumber("1"..string.rep("0", SCHEMA.digitsLen-1)), tonumber(string.rep("9", SCHEMA.digitsLen)))
-		return SCHEMA.cpPrefix..table.GetFirstValue(SCHEMA.rctRanks).."."..digits, true
+		digits = digits or math.random(
+			tonumber("1"..string.rep("0", SCHEMA.digitsLen-1)),
+			tonumber(string.rep("9", SCHEMA.digitsLen))
+		)
+
+		local name = SCHEMA.cpPrefix
+			..table.GetFirstValue(SCHEMA.rctRanks)
+			.."."
+			..digits
+		return name, true
 	else
 		return SCHEMA.cpPrefix..table.GetFirstValue(SCHEMA.rctRanks), true
 	end
+end
+
+function FACTION:onTransfered(client, oldFaction)
+	local digits
+	local inventory = client:getChar():getInv()
+	if (inventory) then
+		for _, item in pairs(inventory:getItems()) do
+			if (item.uniqueID == "cid" and item:getData("id")) then
+				digits = item:getData("id")
+				break
+			end
+		end
+	end
+
+	client:getChar():setName(self:onGetDefaultName(client, digits))
+	hook.Run("PlayerLoadout", client)
 end
 
 FACTION_CP = FACTION.index
