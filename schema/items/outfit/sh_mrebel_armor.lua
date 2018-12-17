e3291d9a6a92a9265d4c031f925b68c2447077df
Brian Hang <hangwithbrian@gmail.com>
2018-12-16 16:11:50 -0800

    Add rebel armor items

---
+++ schema/items/outfit/sh_mrebel_armor.lua
@@ -0,0 +1,10 @@
+ITEM.name = "Resistance Medic Armor"
+ITEM.desc = "Resistance clothing with a red cross on it."
+ITEM.replacements = {
+    {"group01", "group03m"},
+    {"group02", "group03m"},
+    {"group03", "group03m"},
+}
+ITEM.armor = 25
+ITEM.price = 100
+ITEM.flag = "y"
---
+++ schema/items/outfit/sh_rebel_armor.lua
@@ -0,0 +1,10 @@
+ITEM.name = "Resistance Armor"
+ITEM.desc = "Clothing fit for the Resistance."
+ITEM.replacements = {
+    {"group01", "group03"},
+    {"group02", "group03"},
+    {"group03m", "group03"},
+}
+ITEM.armor = 25
+ITEM.price = 100
+ITEM.flag = "y"
